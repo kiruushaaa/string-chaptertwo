dd7712999d54426f19e9b1d9c728958e7b646f50
Kirill <kiruxa4866613@gmail.com>
2020-05-01 20:36:58 +0300

    update test

--- src/by/learn/textparser/runner/Runner.java
+++ src/by/learn/textparser/runner/Runner.java
@@ -1,19 +1,13 @@
 package by.learn.textparser.runner;
 
-import by.learn.textparser.components.Sentence;
 import by.learn.textparser.components.Text;
-import by.learn.textparser.components.Word;
+import by.learn.textparser.utils.Parser;
 import by.learn.textparser.utils.Reader;
-
-import java.util.List;
 
 public class Runner {
     public static void main(String[] args) {
-        String path = "/Users/kirusha/Documents/Java/string-templates/text.txt";
+        String path = "/Users/kirusha/Documents/Java/string-templates/text1.txt";
         Text text = new Text(Reader.fromFile(path));
-        List<Sentence> sentences = text.parse();
-        List<Word> words = sentences.get(0).parse();
-        System.out.println(sentences.get(0));
-        System.out.println(words);
+        System.out.println(Parser.findUniqueWord(text));
     }
 }
