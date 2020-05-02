800438541ca6e45e939eff454732c70894de7afd
Kirill <kiruxa4866613@gmail.com>
2020-05-02 12:41:12 +0300

    test for Task II

---
+++ src/by/learn/textparser/runner/SecondRunner.java
@@ -0,0 +1,15 @@
+package by.learn.textparser.runner;
+
+import by.learn.textparser.components.Text;
+import by.learn.textparser.utils.Parser;
+import by.learn.textparser.utils.Reader;
+
+public class SecondRunner {
+    public static void main(String[] args) {
+        String path = "/Users/kirusha/Documents/Java/string-templates/text1.txt";
+        Text text = new Text(Reader.fromFile(path));
+        System.out.println(text);
+        text = new Text(Parser.getSentences(text));
+        System.out.println(text);
+    }
+}
