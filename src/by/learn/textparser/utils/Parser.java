f7cc37268ab950d7655309ab17580e8057179e59
Kirill <kiruxa4866613@gmail.com>
2020-05-02 12:13:59 +0300

    added universality to findUniqueWord()

--- src/by/learn/textparser/utils/Parser.java
+++ src/by/learn/textparser/utils/Parser.java
@@ -8,12 +8,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Parser {
-    public static List<Word> findUniqueWord(Text text) {
+    public static List<Word> findUniqueWord(Text text, int index) {
         List<Word> uniqueWords = new ArrayList<>();
         List<Sentence> sentences = text.parse();
-        for(Word currentWord : sentences.get(0).parse()) {
+        for(Word currentWord : sentences.get(index).parse()) {
             boolean flag = false;
-            for (int i = 1; i < sentences.size(); i++) {
+            for (int i = 0; i < sentences.size(); i++) {
+                if (i == index) continue;
                 flag = sentences.get(i).parse().contains(currentWord);
             }
             if (!flag) uniqueWords.add(currentWord);
