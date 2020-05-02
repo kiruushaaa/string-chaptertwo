4243037af38f69b058d51415661e6c2bcdfd5231
Kirill <kiruxa4866613@gmail.com>
2020-05-02 17:33:29 +0300

    update test

--- src/by/learn/textparser/runner/Runner.java
+++ src/by/learn/textparser/runner/Runner.java
@@ -6,7 +6,7 @@ import by.learn.textparser.utils.Reader;
 
 public class Runner {
     public static void main(String[] args) {
-        String path = "/Users/kirusha/Documents/Java/string-templates/text1.txt";
+        String path = "/Users/kirusha/Documents/Java/string-templates/text.txt";
         Text text = new Text(Reader.fromFile(path));
         System.out.println(Parser.findUniqueWord(text,0));
     }
